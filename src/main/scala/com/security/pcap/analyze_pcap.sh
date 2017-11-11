3a3ec5b7a7a87e445254ccdb0691f5a3b3b7a930
J. Alexander <xandermoorman@gmail.com>
2017-11-11 17:44:15 -0600

    I don't actually know how to use bash.

--- src/main/scala/com/security/pcap/analyze_pcap.sh
+++ src/main/scala/com/security/pcap/analyze_pcap.sh
@@ -16,8 +16,12 @@ outResult = “out-sanitized%UNIX_TIME%.csv”
 # Run for 35 seconds 
 tcpdump -G 35 -w $firstDump -i eth1
 
+echo pcaps captured by tcpdump stored at $firstDump
+
 tshark -2 -r $1 -T fields -E header=y -E separator=, -E occurrence=a -E quote=d -e frame.time -e ip.version -e ip.id -e ip.len -e ip.proto -e ip.ttl -e ip.flagshttp.request.method" -e http.host -e http.request.version -e http.user_agent -ehttp.server -e http.response.code -e " http.response.phrase “ip.version==" > $fileName
 
 sed '1s/\./_/g;1s/\([^,\n]\{1,\}\)/"\1"/g' $outResult > $outResult
+
+echo CSV from tshark stored in $outResult
 
 scala analyze_pcap.jar $outResult
