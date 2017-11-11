0514993043784070fbaa2f1b4ff8413b6762eb9a
J. Alexander <xandermoorman@gmail.com>
2017-11-11 17:27:05 -0600

    Update AnalyzePcapMain.scala

--- src/main/scala/com/security/pcap/AnalyzePcapMain.scala
+++ src/main/scala/com/security/pcap/AnalyzePcapMain.scala
@@ -3,8 +3,7 @@ package com.security.pcap
 object AnalyzePcapMain {
 
   def main(args: Array[String]): Unit = {
-    // val fileName = "/Users/AlCoolDudeBinRoqui/Documents/pcaps/morningcapture.pcap"
-
+ 
     val pcap = new AutomatePcapAnalysis(args(1))
 
     pcap.run()
