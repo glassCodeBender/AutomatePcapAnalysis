e8853f968c2a29dac77b707a21fc400cf95a349a
J. Alexander <xandermoorman@gmail.com>
2017-11-11 22:04:48 -0600

    Fixed the commands, but not sure if it'll work.

--- src/main/scala/com/security/pcap/analyze_pcap.sh
+++ src/main/scala/com/security/pcap/analyze_pcap.sh
@@ -5,23 +5,22 @@ clear
 Set local
 Call :GetUnixTime UNIX_TIME
 
-firstDump = “raw_pcap%UNIX_TIME%.pcap”
+firstDump = “raw_pcap%UNIX_TIME%.tcpdump”
 fileName = “pcap%UNIX_TIME%.csv”
-outResult = “out-sanitized%UNIX_TIME%.csv”
 
 # Going to write to pcap file.
 # will run this for a set amount of time. 
 # Command to get start time start_time="$(date -u +%s)"
 
-# Run for 35 seconds 
-tcpdump -G 35 -w $firstDump -i eth1
+# If the directory already doesn’t exist, make it. 
+mkdir Documents/pcaps
 
-echo pcaps captured by tcpdump stored at $firstDump
+# Capture the next 20,000 packets
+tcpdump -c 20000 -w Documents/pcaps/$firstDump -i en1
 
-tshark -2 -r $1 -T fields -E header=y -E separator=, -E occurrence=a -E quote=d -e frame.time -e ip.version -e ip.id -e ip.len -e ip.proto -e ip.ttl -e ip.flagshttp.request.method" -e http.host -e http.request.version -e http.user_agent -ehttp.server -e http.response.code -e " http.response.phrase “ip.version==" > $fileName
+# Organize data from pcap file into csv format.
+tshark -2 -r Documents/pcaps/$firstDump -T fields -E header=y -E separator=, -E occurrence=a -E quote=d -e frame.time -e ip.version -e ip.id -e ip.len -e ip.proto -e ip.ttl -e ip.flags -e ip.src -e ip.dst -e icmp.code -e icmp.type -e icmp.resptime -e udp.srcport -e udp.dstport -e dns.id -e dns.qry.type -e dns.resp.type -e dns.qry.name -e dns.a -e tcp.stream -e tcp.seq -e tcp.flags -e tcp.srcport -e tcp.dstport -e http.request.method -e http.host -e http.request.version -e http.user_agent -e http.server -e http.response.code -e http.response.phrase "ip.version==4" > Documents/pcaps/$fileName
 
-sed '1s/\./_/g;1s/\([^,\n]\{1,\}\)/"\1"/g' $outResult > $outResult
+scala analyze_pcap.jar Documents/pcaps/$fileName
 
-echo CSV from tshark stored in $outResult
 
-scala analyze_pcap.jar $outResult
