2060021c12f1d4483cac563f6d533ad71a54c674
J. Alexander <xandermoorman@gmail.com>
2017-11-11 23:36:25 -0600

    Update AnalyzePcapMain.scala

--- src/main/scala/com/security/pcap/AnalyzePcapMain.scala
+++ src/main/scala/com/security/pcap/AnalyzePcapMain.scala
@@ -3,12 +3,13 @@ package com.security.pcap
 object AnalyzePcapMain {
 
   def main(args: Array[String]): Unit = {
- 
-    val pcap = new AutomatePcapAnalysis(args(1))
+
+    val csvFile = io.Source.fromFile("Documents/pcaps/file_reads.txt").mkString.trim
+    val fileName =  System.getProperty("user.dir") + "/" + csvFile
+
+    val pcap = new AutomatePcapAnalysis(fileName)
 
     pcap.run()
-
-
 
   } // END main()
 } // END AnalyzePcapMain object
