eed8ab0d58c5fc339daf03814d54180abecdd2ea
J. Alexander <xandermoorman@gmail.com>
2017-11-12 21:12:33 -0600

    Changed tshark output to remove "" and make tab separated.

--- src/main/scala/com/security/pcap/analyze_pcap.sh
+++ src/main/scala/com/security/pcap/analyze_pcap.sh
@@ -22,6 +22,7 @@ cat “Documents/pcaps/$fileName” > Documents/pcaps/file_reads.txt
 tcpdump -c 40000 -w Documents/pcaps/$firstDump -i en1
 
 # Organize data from pcap file into csv format.
-tshark -2 -r Documents/pcaps/$firstDump -T fields -E header=y -E separator=, -E occurrence=a -E quote=d -e frame.time -e ip.version -e ip.id -e ip.len -e ip.proto -e ip.ttl -e ip.flags -e ip.src -e ip.dst -e icmp.code -e icmp.type -e icmp.resptime -e udp.srcport -e udp.dstport -e dns.id -e dns.qry.type -e dns.resp.type -e dns.qry.name -e dns.a -e tcp.stream -e tcp.seq -e tcp.flags -e tcp.srcport -e tcp.dstport -e http.request.method -e http.host -e http.request.version -e http.user_agent -e http.server -e http.response.code -e http.response.phrase "ip.version==4" > Documents/pcaps/$fileName
+tshark -2 -r Documents/pcaps/$firstDump -T fields -E header=y -E separator=/t -E occurrence=a -E quote=n -e frame.time -e ip.version -e ip.id -e ip.len -e ip.proto -e ip.ttl -e ip.flags -e ip.src -e ip.dst -e icmp.code -e icmp.type -e icmp.resptime -e udp.srcport -e udp.dstport -e dns.id -e dns.qry.type -e dns.resp.type -e dns.qry.name -e dns.a -e tcp.stream -e tcp.seq -e tcp.flags -e tcp.srcport -e tcp.dstport -e http.request.method -e http.host -e http.request.version -e http.user_agent -e http.server -e http.response.code -e http.response.phrase "ip.version==4" > Documents/pcaps/$fileName
 
 scala analyze_pcap.jar
+
